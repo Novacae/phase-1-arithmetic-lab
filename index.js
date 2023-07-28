c395673a0175343d1f49f74fa3732dab78760125
aman githinji <amangithinji09@gmail.com>
2023-07-28 21:55:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+const multiply = num1*num2;
+
+
+let num3 = 123;
+let num4 = 7;
+const mod = num3 % num4;
+
+let max = (1,2,7,5,6,4,7,20);
+max.max();
+
+let random = Number;
+random(Math.random() * any) + 1
+
+
+
+
+//let random = function() {
+   // (Math.floor(Math.random() * any) + 1);
+
+
